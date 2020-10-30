855498058ec98c709fcc5607b50e53790ebf5c3d
Gabriel T <gabriel.thiagohs@gmail.com>
2020-10-29 21:33:47 -0300

    estrutura processador

--- processador.c
+++ processador.c
@@ -6,68 +6,49 @@
 #include "tipos_instrucoes/i_types.h"
 #include "conversor.h"
 
+void executeScoreboarding();
+void writeResult();
+void readOperands();
+void executeOperands();
+void preencheFU();
+bool executeIssue();
+bool verifyIfAllWasWrited();
+UnitInstruction_t verifyDependency();
+
 unsigned int clock;
 
 // EXEMPLO INSTRUCAO //
 //  ADD  = R[RD] = R[RS] + R[RT]
 // 
-//  ADDI = R[RT] = [RS]  + Imediate
+//  ADDI = R[RS] = [RT]  + Imediate
 
-
-UnitInstruction_t verifyDependency(functional_unit_status_table_t *fu_status_table, UnitInstruction_t typeOp, 
-                  unsigned int fjAtual, unsigned int fkAtual, unsigned int opcode)
+void executeScoreboarding(
+    unsigned int numberOfInstructions,
+    functional_unit_status_table_t *fu_status_table,
+    instruction_status_t *inst_status_table,
+    register_result_status_table_t *rr_status_table)
 {
-  UnitInstruction_t dependenciaQJ, dependenciaQK;
-  
-  dependenciaQJ = empty;
-  dependenciaQK = empty;
+  // inicializa
+  clock = 1;
+  unsigned int instAtual = 0;
+  bool allWasWrited = false;
 
-  // fiquei meio incerto se isso realmente da certo, pq ele verifica na ordem da tabela 
-  // e nao das instrucoes, sera q tem como criar uma dependecia que não é a dele realmente
+  while (!allWasWrited)
+  {
+    printf("\n---------Inicio clock--------\n");
+    printf("Busy Add Ocupado: %s\n", fu_status_table->add.busy ? "Sim" : "Não" );
 
-  if (fu_status_table->add.busy && typeOp != ADD_FU_DECIMAL){ 
-    if (fu_status_table->add.dest_Fi == fjAtual)
-      dependenciaQJ = ADD_FU_DECIMAL;
+    if (executeIssue(inst_status_table[instAtual].instruction, inst_status_table, fu_status_table, rr_status_table, instAtual))
+      instAtual++; // se a atual iniciou pra issue a inst pode ir pra proxima
 
-    if (fu_status_table->add.dest_Fi == fkAtual)
-      dependenciaQK = ADD_FU_DECIMAL;
+    readOperands(inst_status_table[0].instruction);
+    executeOperands(inst_status_table[0].instruction);
+    writeResult(inst_status_table[0].instruction);
+
+    printf("----------Fim clock----------\n");
+    allWasWrited = verifyIfAllWasWrited(inst_status_table, 2);
+    clock += 1;
   }
-
-  if (fu_status_table->mult1.busy && typeOp != MULT1_FU_DECIMAL){
-    if (fu_status_table->mult1.dest_Fi == fjAtual)
-      dependenciaQJ = MULT1_FU_DECIMAL;
-
-    if (fu_status_table->mult1.dest_Fi == fkAtual)
-      dependenciaQK = MULT1_FU_DECIMAL;
-  }
-
-  if (fu_status_table->mult2.busy && typeOp != MULT2_FU_DECIMAL){
-    if (fu_status_table->mult2.dest_Fi == fjAtual)
-      dependenciaQJ = MULT2_FU_DECIMAL;
-
-    if (fu_status_table->mult2.dest_Fi == fkAtual)
-      dependenciaQK = MULT2_FU_DECIMAL;
-  }
-
-  if (fu_status_table->divide.busy && typeOp != DIVIDE_FU_DECIMAL){
-    if (fu_status_table->divide.dest_Fi == fjAtual)
-      dependenciaQJ = DIVIDE_FU_DECIMAL;
-
-    if (fu_status_table->divide.dest_Fi == fkAtual)
-      dependenciaQK = DIVIDE_FU_DECIMAL;  
-  }
-
-  if (fu_status_table->log.busy && typeOp != LOG_FU_DECIMAL){
-    if (fu_status_table->log.dest_Fi == fjAtual)
-      dependenciaQJ = LOG_FU_DECIMAL;
-
-    if (fu_status_table->log.dest_Fi == fkAtual)
-      dependenciaQK = LOG_FU_DECIMAL;
-  }
-
-  printf("\n--> dependencia QK: %d\n--> dependencia QJ: %d\n", dependenciaQK, dependenciaQJ);
-
-  return dependenciaQK, dependenciaQJ;
 }
 
 void preencheFU(unsigned int instruction, functional_unit_status_table_t *fu_status_table)
@@ -145,7 +126,7 @@ bool verifyIfAllWasWrited(instruction_status_t *inst_status_table, unsigned int 
 }
 
 bool executeIssue(unsigned int instruction, instruction_status_t *inst_status_table,
-                  functional_unit_status_table_t *fu_status_table, register_result_status_table_t *rr_status_table)
+                  functional_unit_status_table_t *fu_status_table, register_result_status_table_t *rr_status_table, unsigned int instAtual)
 {
   unsigned int opcode = desconverteOp(instruction);
   unsigned int funct = desconverteFunct(instruction);
@@ -158,13 +139,69 @@ bool executeIssue(unsigned int instruction, instruction_status_t *inst_status_ta
   printf("Segue? %s\n", canProceed ? "Sim" : "Não");
   if (canProceed)
   {
-    inst_status_table->issue = clock;                // atualiza o clock no status na tabela d inst
+    inst_status_table[instAtual].issue = clock;                // atualiza o clock no status na tabela d inst
     preencheFU(instruction, fu_status_table);        // preenche tabela FU
     preencheRegStatus(instruction, rr_status_table, isR); // preenche tab dos Reg
     return true;
   }
   else
     return false;
+}
+
+UnitInstruction_t verifyDependency(functional_unit_status_table_t *fu_status_table, UnitInstruction_t typeOp, 
+                  unsigned int fjAtual, unsigned int fkAtual, unsigned int opcode)
+{
+  UnitInstruction_t dependenciaQJ, dependenciaQK;
+  
+  dependenciaQJ = empty;
+  dependenciaQK = empty;
+
+  // fiquei meio incerto se isso realmente da certo, pq ele verifica na ordem da tabela 
+  // e nao das instrucoes, sera q tem como criar uma dependecia que não é a dele realmente
+
+  if (fu_status_table->add.busy && typeOp != ADD_FU_DECIMAL){ 
+    if (fu_status_table->add.dest_Fi == fjAtual)
+      dependenciaQJ = ADD_FU_DECIMAL;
+
+    if (fu_status_table->add.dest_Fi == fkAtual)
+      dependenciaQK = ADD_FU_DECIMAL;
+  }
+
+  if (fu_status_table->mult1.busy && typeOp != MULT1_FU_DECIMAL){
+    if (fu_status_table->mult1.dest_Fi == fjAtual)
+      dependenciaQJ = MULT1_FU_DECIMAL;
+
+    if (fu_status_table->mult1.dest_Fi == fkAtual)
+      dependenciaQK = MULT1_FU_DECIMAL;
+  }
+
+  if (fu_status_table->mult2.busy && typeOp != MULT2_FU_DECIMAL){
+    if (fu_status_table->mult2.dest_Fi == fjAtual)
+      dependenciaQJ = MULT2_FU_DECIMAL;
+
+    if (fu_status_table->mult2.dest_Fi == fkAtual)
+      dependenciaQK = MULT2_FU_DECIMAL;
+  }
+
+  if (fu_status_table->divide.busy && typeOp != DIVIDE_FU_DECIMAL){
+    if (fu_status_table->divide.dest_Fi == fjAtual)
+      dependenciaQJ = DIVIDE_FU_DECIMAL;
+
+    if (fu_status_table->divide.dest_Fi == fkAtual)
+      dependenciaQK = DIVIDE_FU_DECIMAL;  
+  }
+
+  if (fu_status_table->log.busy && typeOp != LOG_FU_DECIMAL){
+    if (fu_status_table->log.dest_Fi == fjAtual)
+      dependenciaQJ = LOG_FU_DECIMAL;
+
+    if (fu_status_table->log.dest_Fi == fkAtual)
+      dependenciaQK = LOG_FU_DECIMAL;
+  }
+
+  printf("\n--> dependencia QK: %d\n--> dependencia QJ: %d\n", dependenciaQK, dependenciaQJ);
+
+  return dependenciaQK, dependenciaQJ;
 }
 
 void readOperands(unsigned int instruction)
@@ -179,48 +216,60 @@ void writeResult(unsigned int instruction)
 {
 }
 
-void executeScoreboarding(
-    unsigned int numberOfInstructions,
-    functional_unit_status_table_t *fu_status_table,
-    instruction_status_t *inst_status_table,
-    register_result_status_table_t *rr_status_table)
-{
-  // inicializa
-  clock = 1;
-  unsigned int instAtual = 0;
-  bool allWasWrited = false;
+  /*
+    Help do scoreboarding
+    ------------------------------------------------------------------
+    1-
+    Issue - instruções de decodificação e verificação de riscos estruturais
+      Condições de espera:
+        (1) o FU necessário estar livre; 
+        (2) nenhuma outra instrução escreve para o mesmo destino de registro (para evitar WAW)
 
-  while (!allWasWrited)
-  {
-    printf("\n---------Inicio clock--------\n");
-    printf("Busy Add Ocupado: %s\n", fu_status_table->add.busy ? "Sim" : "Não" );
+      Ações: 
+        (1) a instrução segue para a FU; 
+        (2) o placar atualiza sua estrutura de dados interna. Se uma instrução for interrompida neste estágio, nenhuma outra instrução poderá prosseguir.
+    ------------------------------------------------------------------
+    2-
+    Read Operands - espere até que não haja riscos de dados, então leia os operandos
+      Condições de espera: 
+        (1) todos os operandos de origem estão disponíveis
+      
+      Ações: 
+        (1) a unidade de função lê operandos de registro e inicia a execução do próximo ciclo
+    ------------------------------------------------------------------
+    3-
+    Execution - opera em operandos (EX)
+      Ações: 
+        (1) A unidade funcional inicia a execução ao receber operandos. 
+            Quando o resultado estiver pronto, ele notifica o placar de que concluiu a execução.
+    ------------------------------------------------------------------
+    4-
+    Write Resulta - execução final (WB)
+      Condição de espera: 
+            nenhuma outra instrução / FU vai ler o registrador de destino da instrução
+      Ações: 
+            Escreva o registro e atualize o placar
+            
+            Exemplo de WAR: DIVD F0, F2, F4ADDD
+                            F10, F0, F8
+                            SUB DF8, F8, F14
+            
+            O placar CDC 6600 paralisaria o SUBD até que o ADDD leia operandos
+    ------------------------------------------------------------------
 
-    if (executeIssue(inst_status_table[instAtual].instruction, inst_status_table, fu_status_table, rr_status_table))
-      instAtual++; // se a atual iniciou pra issue a inst pode ir pra proxima
+     unsigned int opcode = desconverteOp(instruction);
+     unsigned int rs = desconverteRs(instruction);
+     unsigned int rt = desconverteRt(instruction);
 
-    printf("Busy Add Ocupado: %s\n", fu_status_table->add.busy ? "Sim" : "Não" );
+     if (opcode == R)
+     {
+       unsigned int rd = desconverteRd(instruction);
+       unsigned int shamt = desconverteShamt(instruction);
+       unsigned int funct = desconverteFunct(instruction);
+     }
+     else //tipo I
+     {
+      unsigned int immediate = desconverteImmediate(instruction);
+   }
 
-    readOperands(inst_status_table[0].instruction);
-    executeOperands(inst_status_table[0].instruction);
-    writeResult(inst_status_table[0].instruction);
-
-    printf("----------Fim clock----------\n");
-    allWasWrited = verifyIfAllWasWrited(inst_status_table, 2);
-    clock += 1;
-  }
-
-  // unsigned int opcode = desconverteOp(instruction);
-  // unsigned int rs = desconverteRs(instruction);
-  // unsigned int rt = desconverteRt(instruction);
-
-  // if (opcode == R)
-  // {
-  //   unsigned int rd = desconverteRd(instruction);
-  //   unsigned int shamt = desconverteShamt(instruction);
-  //   unsigned int funct = desconverteFunct(instruction);
-  // }
-  // else //tipo I
-  // {
-  //   unsigned int immediate = desconverteImmediate(instruction);
-  // }
-}
+  */
