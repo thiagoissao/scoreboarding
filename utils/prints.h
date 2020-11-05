3d4a190522932394b5e8ba6374132c915f05afb1
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2020-11-05 10:51:06 -0300

    Ajusta nome dos registradores'

--- utils/prints.h
+++ utils/prints.h
@@ -44,7 +44,7 @@ void print_instructions_complete(instruction_status_t *table, int size)
        {
               if (isR(table[i].instruction))
               {
-                     printf("%4s %3s, %3s, %3s\t  %1i\t\t %4i\t %12i\t %12i\n",
+                     printf("%4s $%s, $%s, $%s\t  %1i\t\t %4i\t %12i\t %12i\n",
                             opcodeToString(desconverteFunct(table[i].instruction)),
                             registerToString(desconverteRd(table[i].instruction)),
                             registerToString(desconverteRs(table[i].instruction)),
@@ -56,7 +56,7 @@ void print_instructions_complete(instruction_status_t *table, int size)
               }
               else
               {
-                     printf("%4s $%3s,$%3s, %3i\t  %1i\t\t %4i\t %12i\t %12i\n",
+                     printf("%4s $%s, $%s, %3i\t  %1i\t\t %4i\t %12i\t %12i\n",
                             opcodeToString(desconverteOp(table[i].instruction)),
                             registerToString(desconverteRs(table[i].instruction)),
                             registerToString(desconverteRt(table[i].instruction)),
