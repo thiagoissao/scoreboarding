077cccf5ae9e45cfb976f39d77610136c8ef9112
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2020-12-10 10:36:25 -0300

    Criação das threads

--- main.c
+++ main.c
@@ -15,12 +15,30 @@
 #include "config/config.h"
 #include "config/config_converter.h"
 
+typedef struct
+{
+    int id, number_of_configs, number_instructions;
+    char *path_program, *path_destiny;
+    config_t *configurations;
+} thread_arg, *ptr_thread_arg;
+
+void *run(void *arg)
+{
+    ptr_thread_arg targ = (ptr_thread_arg)arg;
+    execute_pipeline(
+        targ->path_destiny,
+        targ->path_program,
+        targ->number_of_configs,
+        targ->configurations,
+        targ->number_instructions);
+}
+
 int main(int argc, char *argv[])
 {
     int option;
-    char *config = NULL;  //"./examples/config.txt"
-    char *archive = NULL; //"./examples/mnemoniosMult.txt"
-    char *destino = NULL;
+    char *config = NULL;       //"./examples/config.txt"
+    char *path_program = NULL; //"./examples/mnemoniosMult.txt"
+    char *path_destiny = NULL;
     FILE *archive_destiny;
     int number_instructions = 0;
 
@@ -37,12 +55,12 @@ int main(int argc, char *argv[])
             break;
 
         case 'p':
-            archive = optarg;
+            path_program = optarg;
             break;
 
         case 'o':
-            destino = optarg;
-            archive_destiny = fopen(destino, "w");
+            path_destiny = optarg;
+            archive_destiny = fopen(path_destiny, "w");
             if (archive_destiny == NULL)
             {
                 printf("Erro no nome do arquivo destino!\n");
@@ -63,11 +81,11 @@ int main(int argc, char *argv[])
             exit(1);
         }
     }
-    if (number_instructions <= 0 || !config || !archive || !destino)
+    if (number_instructions <= 0 || !config || !path_program || !path_destiny)
     {
         printf("Parametrização Incorreta!\n");
         printf("Utilize:\n");
-        printf("\t./executavel -n <qtd_de_instrucao> -c <arq_configuracao.txt> -o <arq_destino> -p <arq_instrucoes.txt>\n");
+        printf("\t./executavel -n <qtd_de_instrucao> -c <arq_configuracao.txt> -o <arq_path_destiny> -p <arq_instrucoes.txt>\n");
         exit(1);
     }
 
@@ -76,14 +94,30 @@ int main(int argc, char *argv[])
     config_t configurations[number_of_configs];
     config_converter(config, configurations);
 
-    execute_pipeline(
-        destino,
-        archive,
-        number_of_configs,
-        configurations,
-        number_instructions);
+    pthread_t threads[2];
+    thread_arg arguments[2];
 
-    printf("Simulação concluida!\nArquivo <%s> criado!\n", destino);
+    arguments[0].id = 0;
+    arguments[0].configurations = configurations;
+    arguments[0].number_instructions = number_instructions;
+    arguments[0].number_of_configs = number_of_configs;
+    arguments[0].path_destiny = path_destiny;
+    arguments[0].path_program = path_program;
+
+    arguments[1].id = 1;
+    arguments[1].configurations = configurations;
+    arguments[1].number_instructions = number_instructions;
+    arguments[1].number_of_configs = number_of_configs;
+    arguments[1].path_destiny = path_destiny;
+    arguments[1].path_program = path_program;
+
+    pthread_create(&(threads[0]), NULL, run, &(arguments[0]));
+    pthread_create(&(threads[1]), NULL, run, &(arguments[1]));
+
+    pthread_join(threads[0], NULL);
+    pthread_join(threads[1], NULL);
+
+    printf("Simulação concluida!\nArquivo <%s> criado!\n", path_destiny);
 
     return 0;
 }
