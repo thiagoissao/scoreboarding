2337d9ec14247afa948fbe88fc928d30154b3a1d
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2020-12-10 09:54:47 -0300

    remove variáveis globais

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <pthread.h>
 #include "conversor.h"
 #include "processador.c"
 #include "components/functional_unit_status.h"
@@ -13,10 +14,6 @@
 #include "prints/prints.h"
 #include "config/config.h"
 #include "config/config_converter.h"
-
-// #define PROGRAM "mnemonios.txt"
-// #define CONFIG "config.txt"
-// #define CICLOS 18
 
 int main(int argc, char *argv[])
 {
@@ -79,56 +76,14 @@ int main(int argc, char *argv[])
     config_t configurations[number_of_configs];
     config_converter(config, configurations);
 
-    // Cria na memória um array com o inteiro de cada instrução
-    unsigned int instruction_set[number_instructions];
-
-    // Converte o conjunto das instruções para inteiro e armazena no array passado por referência
-    converter(archive, instruction_set);
-
-    // Status das unidades funcionais e inicialização
-    functional_unit_status_table_t *fu_status_table = (functional_unit_status_table_t *)malloc(sizeof(functional_unit_status_table_t));
-    init_functional_unit_status_table(fu_status_table);
-
-    // Status das instruções e inicialização
-    instruction_status_t inst_status_table[number_instructions];
-    init_instruction_status_table(inst_status_table, instruction_set, number_instructions);
-
-    // Status dos registradores e inicialização
-    register_result_status_table_t *rr_status_table = (register_result_status_table_t *)malloc(sizeof(register_result_status_table_t));
-    init_register_status_table(rr_status_table);
-
-    // Banco de registradores
-    register_database_t *register_database = (register_database_t *)malloc(sizeof(register_database_t));
-    init_register_database(register_database);
-
     execute_pipeline(
         destino,
+        archive,
         number_of_configs,
         configurations,
-        number_instructions,
-        fu_status_table,
-        inst_status_table,
-        rr_status_table,
-        register_database);
+        number_instructions);
 
     printf("Simulação concluida!\nArquivo <%s> criado!\n", destino);
-    free(fu_status_table);
-    free(rr_status_table);
-    free(register_database);
+
     return 0;
 }
-
-/*
-* - main.c  => Controla o scoreboarding, aqui são utilizadas todas as structs, conversões, etc.
-* - conversor.h => ler o arquivo de instrucoes e salvar na memória, porém o papel de salvar na memória deve ser atribuida a main.c. Então a ideia para o conversor.h é receber valores e devolver esses valores prontos.
-* - Cada "tabela" do scoreboarding deve ser separado em arquivos diferentes para facilitar a leitura posteriormente, organizar cada "tabela" em structs e construir funcões auxiliares para cada tipo de processamento. Lembrando que quem deve utilizar essas structs e funcões auxiliares é a main.c
-* - Existe um arquivo chamado processador.c. Seu papel é organizar essas "tabelas" de forma que eles trabalhem em sintonia. Acho que uma forma legal de arquiteturar esse arquivo é criar métodos para a main.c, pois dessa forma toda a lógica de execucão fica por conta da main. Por exemplo, a instrucão atual é um ADD, então o processador.c deve possuir uma funcão de ADD.
-*    Sugestão de uso: 
-*      ENQUANTO existir instrucões FACA
-*         INTRUCAO_ATUAL = INSTRUCOES[i]
-*         SE INSTRUCAO_ATUAL === ADD ENTAO
-*            processador.add(INSTRUCAO_ATUAL)
-*         SE INSTRUCAO_ATUAL === MUL ENTAO
-*            processador.mul(INSTRUCAO_ATUAL)
-*          ....
-*/
